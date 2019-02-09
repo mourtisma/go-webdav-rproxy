b796681d99611289a0580dcc617c09114bcbbccf
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2019-02-09 19:45:08 +0100

    Describe the proxy code

--- proxy/src/proxy.go
+++ proxy/src/proxy.go
@@ -10,16 +10,30 @@ import (
 )
 
 func Forward(w http.ResponseWriter, r *http.Request) {
+	
+	// Instantiate the client 
 	client := &http.Client{}
+
+	// Route the request to the webdav server in the webdav-apache container
 	url := fmt.Sprintf("http://webdav-apache/webdav/%s", r.URL.Path)
+
+	// Create a new request with the same method and the same body as the one hitting the proxy
 	req, _ := http.NewRequest(r.Method, url, r.Body)
+
+	// Inject the basic auth credentials
 	req.SetBasicAuth(os.Getenv("USERNAME"), os.Getenv("PASSWORD"))
+
+	// Copy the headers
 	for key, value := range r.Header {
 		req.Header.Set(key, strings.Join(value, ","))
 	}
+
+	// Fire the reaquest
 	resp, _ := client.Do(req)
 
 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+
+	// Write the headers and the response back to the client
 	for key, value := range resp.Header {
 		w.Header().Set(key, strings.Join(value, ","))
 	}

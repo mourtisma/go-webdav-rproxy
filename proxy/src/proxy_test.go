5f83c4dbb1a4e66deec59d6fd27c8b72e8c57284
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2019-02-24 19:20:38 +0100

    Add testing step to CI and modify the go test

--- proxy/src/proxy_test.go
+++ proxy/src/proxy_test.go
@@ -21,15 +21,9 @@ func TestForward(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	// Check the status code
-	if status := rr.Code; status != http.StatusOK {
+	if status := rr.Code; status != http.StatusNotFound {
 		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
+			status, http.StatusNotFound)
 	}
 
-	// Check the response body
-	expected := "/path"
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
-	}
 }
